c8a511b896adcaf04092cf74311c7b621c733ccf
CORP\Shiva.Marimallaiah <shivaprasadcool06@gmail.com>
2017-01-27 08:45:06 +0530

    Added changes to the Generic class.

---
+++ MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD
+{
+    public class MyList<T> 
+    {
+        private int size;
+
+        private T[] items;
+
+        public MyList(int size)
+        {
+            this.size = size;
+            this.items = new T[size];
+        }
+        
+        public T this[int index]    // Indexer declaration  
+        {
+            get
+            {
+                var item = items[index];
+                return item;
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+    }
+}
